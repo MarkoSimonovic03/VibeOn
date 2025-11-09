cc37ec4f92e9df068b6b0ec4c3287eb9362b5a0d
Ognjen-Stevic <ognjen.stevic@gmail.com>
2025-11-09 12:04:16 +0100

    .

--- src/main/java/com/metropolitan/VibeOn/VibeOnApplication.java
+++ src/main/java/com/metropolitan/VibeOn/VibeOnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VibeOnApplication {
 
     public static void main(String[] args) {
+		//probvaaaaa
         SpringApplication.run(VibeOnApplication.class, args);
     }
 }
