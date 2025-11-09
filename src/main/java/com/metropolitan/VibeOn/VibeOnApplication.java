b829b2d1f058ae4e49cb94e05c39b06c6bb53a11
Marko Simonovic <max2001car@gmail.com>
2025-11-09 12:04:04 +0100

    initial commit

--- src/main/java/com/metropolitan/VibeOn/VibeOnApplication.java
+++ src/main/java/com/metropolitan/VibeOn/VibeOnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VibeOnApplication {
 
     public static void main(String[] args) {
+        //proba push
         SpringApplication.run(VibeOnApplication.class, args);
     }
 }
