b2ffe0d0510797a2406cbe37946c2d35640f750a
Marko Simonovic <max2001car@gmail.com>
2025-11-09 11:43:24 +0100

    initial commit

---
+++ src/main/java/com/metropolitan/VibeOn/VibeOnApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.VibeOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metropolitan/VibeOn/VibeOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.VibeOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
